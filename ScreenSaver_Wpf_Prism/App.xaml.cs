3bae1e34eee3449788ef9da0b02de5a3b798c9fd
Jinge Zhang <jinge_windows@hotmail.com>
2023-07-04 20:09:27 +1000

    Add project files.

---
+++ ScreenSaver_Wpf_Prism/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Prism.Ioc;
+using ScreenSaver_Wpf_Prism.Views;
+using System;
+using System.Windows;
+
+namespace ScreenSaver_Wpf_Prism
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            //return Container.Resolve<MainWindow>();
+            return null;
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+
+        /// <summary>
+        /// Startup different window depending on what parameter be passed in. (Example: App.exe Hello, Hello will be passed in.)
+        /// </summary>
+        /// <param name="e">Program startup parameter.</param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            if (e.Args.Length == 1)
+            {
+                InitializeShell(Container.Resolve<SettingWindow>());
+            }
+            else
+            {
+                InitializeShell(Container.Resolve<MainWindow>());
+            }
+
+            OnInitialized();
+        }
+    }
+}
---
+++ ScreenSaver_Wpf_Prism/ViewModels/SettingWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+
+namespace ScreenSaver_Wpf_Prism.ViewModels
+{
+    public class SettingWindowViewModel : BindableBase
+    {
+        private string _title = "Prism Application";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+    }
+}
